325e6a5d5dd92145ed23e73c0440676e3c8561e2
AljoschaMeyer <mail@aljoscha-meyer.de>
2018-08-24 10:08:11 +0200

    Add error for invalid map keys

--- src/hsdt.h
+++ src/hsdt.h
@@ -58,6 +58,7 @@ typedef enum {
   HSDT_ERR_UTF8, /* An utf8 string conains invalid data */
   HSDT_ERR_INVALID_NAN, /* A float is an NaN value other than 0xf97e00 */
   HSDT_ERR_DUPLICATE_KEY, /* A map contains the same key multiple times */
+  HSDT_ERR_UTF8_KEY, /* A map contains a key that is not a utf8 string */
   HSDT_ERR_CANONIC_LENGTH, /* The length of a collection or array is not given int the canonical format */
   HSDT_ERR_CANONIC_ORDER /* The keys of a map are not sorted correctly */
 } HSDT_ERR;
